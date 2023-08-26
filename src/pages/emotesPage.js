1a1180f97427a07977789f494c82906b27f4646c
forgjayjay <edlink2004@gmail.com>
2023-08-26 11:46:49 +0200

    started working on pagination for emotes

--- src/pages/emotesPage.js
+++ src/pages/emotesPage.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from 'react';
 function EmotesPage() {
   const [imageNames, setImageNames] = useState([]);
   const [emotes, setEmotes] = useState([]);
+  const [pages, setPages] = useState(0);
 
   useEffect(() => {
     fetch('http://localhost:3001/images')
@@ -22,7 +23,20 @@ function EmotesPage() {
     imageNames.map((imageName)=>{
       emotesArray.push(emoteContainer(imageName));
     });
-    return () => setEmotes(emotesArray);
+    
+    for (let index = 0; index < 100; index++) {
+      emotesArray.push(fakeEmoteContainer("fakeName"));
+    }
+
+    emotesArray.sort();
+    const emotesArraySize = emotesArray.length;
+    if(emotesArraySize / 40 > 0){
+      setPages(emotesArraySize/40 | 0);
+    }
+
+    let trimmedEmotesArray = emotesArray.slice(0,40);
+
+    return () => setEmotes(trimmedEmotesArray);
   });
   
   return (
@@ -33,19 +47,33 @@ function EmotesPage() {
           emotes
         }
       </div>
+      <p className='block border border-cyan-400 w-[100%] h-[7%] mt-auto'>{pages}</p>
     </div>
     );
 }
 
-function emoteContainer(emoteName) {
+function fakeEmoteContainer(emoteName) {
   return(
     <div className="h-[150px] w-[150px] bg-zinc-900 flex flex-wrap items-center justify-center m-auto border border-cyan-800 hover:border-slate-400">
-      <Emote 
-        emote={
-          {name: emoteName}
-        }
-      />
-    </div>
+        <div className='flex flex-wrap flex-col items-center justify-center m-auto h-full w-full'>  
+            <img style={{
+                width: 112, height: 112
+            }} class="object-cover" alt={emoteName} />
+            <p>{emoteName}</p>
+        </div>    
+      </div>
+  );
+}
+
+function emoteContainer(emoteName) {
+  return(
+      <div className="h-[150px] w-[150px] bg-zinc-900 flex flex-wrap items-center justify-center m-auto border border-cyan-800 hover:border-slate-400">
+        <Emote 
+          emote={
+            {name: emoteName}
+          }
+        />
+      </div>    
   );
 }
 
